3b7a7bcad1f70a395abf23a67958a5742353a42e
Gareth <blackfireweb@gmail.com>
2022-01-19 20:02:10 +0000

    First commit

---
+++ Source/HarmonyPatches/PsychicDryads.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using PsychicAnimals;
+using RimWorld;
+using Verse;
+
+namespace Replace_Stuff_Compatibility.HarmonyPatches
+{
+	[HarmonyPatch(typeof(HelperClass), "CanBeAssignedToTrain")]
+	public class PsychicDryads
+	{
+		static bool Prefix(ref bool __result, TrainableDef td, ThingDef animal)
+		{
+			if (td == TrainableDefOf.Release && animal.race.Dryad)
+			{
+				__result = true;
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
---
+++ Source/PsychicDryadsPatch.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace PsychicDryadsPatch
+{
+    public class Mod: Verse.Mod
+    {
+        public Mod(ModContentPack content) : base(content)
+        {
+#if DEBUG
+            Harmony.DEBUG = true;
+#endif
+            new Harmony("Garethp.rimworld.PsychicDryadsPatch.main").PatchAll();
+        }
+    }
+}
